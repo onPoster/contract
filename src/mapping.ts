c7c9674a0ddaa15bbe6fa473bb2c27187eff88e5
Auryn Macmillan <aurynmacmillan@gmail.com>
2020-07-28 00:13:18 -0400

    add subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,6 +5,6 @@ export function handleNewPost(event: newPost): void {
   let post = new Post(event.params.id.toHex())
   post.owner = event.params.owner
   post.type = event.params.type
-  post.post = event.params.post
+  post.content = event.params.content
   post.save()
 }
