3b0eeade51e969255ae9d9dec344d896f6ee4ba2
Auryn Macmillan <aurynmacmillan@gmail.com>
2020-09-15 10:12:56 -0400

    make uid=1000(auryn) gid=1000(auryn) groups=1000(auryn),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),118(lpadmin),128(sambashare),999(docker) unique

--- contracts/Poster.sol
+++ contracts/Poster.sol
@@ -1,11 +1,10 @@
 pragma solidity ^0.6.4;
 
 contract Poster {
+  event newPost(bytes32 id, address poster, string content);
 
- event newPost(uint id, address poster, string content);
-
- // Emit content as a json string.
- function post(uint id, string memory content) public {
-   emit newPost(id, msg.sender, content);
- }
+  function post(string memory content) public {
+    bytes32 id = keccak256(abi.encodePacked(msg.sender, blockhash(block.number - 1),gasleft()));
+    emit newPost(id, msg.sender, content);
+  }
 }
